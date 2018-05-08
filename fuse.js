85e7c8ef4c2f878439e1ca9378da310b6eb702e5
Daniel Bornstrand <daniel.bornstrand@gloot.com>
2018-05-08 20:59:20 +0200

    Added sparky

--- fuse.js
+++ fuse.js
@@ -1,20 +1,31 @@
-const { FuseBox, WebIndexPlugin, RawPlugin, ReplacePlugin } = require("fuse-box");
+const { FuseBox, WebIndexPlugin, RawPlugin, ReplacePlugin, QuantumPlugin, Sparky } = require("fuse-box");
 
-const fuse = FuseBox.init({
-    homeDir : "src",
-    target : 'browser@es6',
-    output : "dist/$name.js",
-    useTypescriptCompiler : true,
-    plugins : [
-        WebIndexPlugin({ template: 'src/index.html'}),
-        RawPlugin(['.vert', '.frag']),
-        ReplacePlugin({
-          'CANVAS_RENDERER': JSON.stringify(true),
-          'WEBGL_RENDERER': JSON.stringify(true),
-        }),
+Sparky.task("default", ['clean', 'copy-assets'], devServer);
+Sparky.task('clean', () => Sparky.src('/dist').clean('/dist'));
+Sparky.task('copy-assets', () => Sparky.src("assets/**/**.*", { base: './' }).dest('./dist'));
+
+function devServer() {
+  const fuse = init(false);
+  fuse.dev();
+  fuse.bundle("app").instructions(" > index.js").hmr().watch()
+  fuse.run();
+}
+
+function init(prod) {
+  return FuseBox.init({
+    homeDir: 'src',
+    target: 'browser@es6',
+    output: 'dist/$name.js',
+    useTypescriptCompiler: true,
+    plugins: [
+      WebIndexPlugin({ template: 'src/index.html' }),
+      RawPlugin(['.vert', '.frag']),
+      ReplacePlugin({
+        'CANVAS_RENDERER': JSON.stringify(true),
+        'WEBGL_RENDERER': JSON.stringify(true),
+      }),
+      prod && QuantumPlugin({ uglify: true }),
     ]
-});
+  });
 
-fuse.dev(); // launch http server
-fuse.bundle("app").instructions(" > index.js").hmr().watch()
-fuse.run();
+}
